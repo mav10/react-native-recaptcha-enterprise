079cc485cc9803d6521db22d354a97d4eda00832
maxim vasin <maxim.vasin@mcc-tomsk.de>
2022-12-21 17:42:15 +0700

    feat: initial commit to android folder

--- android/src/main/java/com/recaptchaenterprise/RecaptchaEnterpriseModule.java
+++ android/src/main/java/com/recaptchaenterprise/RecaptchaEnterpriseModule.java
@@ -2,11 +2,19 @@ package com.recaptchaenterprise;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.recaptcha.Recaptcha;
+import com.google.android.recaptcha.RecaptchaTasksClient;
+
+import java.util.List;
+import java.util.concurrent.Executor;
 
 @ReactModule(name = RecaptchaEnterpriseModule.NAME)
 public class RecaptchaEnterpriseModule extends ReactContextBaseJavaModule {
@@ -21,6 +29,36 @@ public class RecaptchaEnterpriseModule extends ReactContextBaseJavaModule {
   public String getName() {
     return NAME;
   }
+
+  private void initializeRecaptchaClient() {
+    Recaptcha
+      .getTasksClient(this, "YOUR_SITE_KEY")
+      .addOnSuccessListener(
+        (Executor) this,
+        new OnSuccessListener<RecaptchaTasksClient>() {
+          @Override
+          public void onSuccess(RecaptchaTasksClient client) {
+//            MainActivity.this.recaptchaTasksClient = client;
+          }
+        })
+      .addOnFailureListener(
+        (Executor) this,
+        new OnFailureListener() {
+          @Override
+          public void onFailure(@NonNull Exception e) {
+            // Handle communication errors ...
+            // See "Handle communication errors" section
+          }
+        });
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules() {
+    return createNativeModules();
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {}
 
 
   // Example method

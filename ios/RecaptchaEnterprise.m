97d8446581d7085d50d3d0244132db96220a5ecc
Maxim Vasin <maxim.vasin@mcc-tomsk.de>
2022-12-22 23:42:11 +0700

    feat: init ios project

--- ios/RecaptchaEnterprise.m
+++ ios/RecaptchaEnterprise.m
@@ -2,7 +2,11 @@
 
 @interface RCT_EXTERN_MODULE(RecaptchaEnterprise, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+RCT_EXTERN_METHOD(initializeRecaptcha:(NSString*)siteKey
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(executeAction:(NSString*)actionName
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
--- ios/RecaptchaEnterprise.swift
+++ ios/RecaptchaEnterprise.swift
@@ -1,8 +1,26 @@
+import RecaptchaEnterprise
+
 @objc(RecaptchaEnterprise)
 class RecaptchaEnterprise: NSObject {
+    var recaptchaClient: RecaptchaClient?
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+    @objc(initializeRecaptcha:withResolver:withRejecter:)
+    func initializeRecaptcha(siteKey: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        Task {
+          let (recaptchaClient, error) = await Recaptcha.getClient(siteKey: siteKey)
+          if let recaptchaClient = recaptchaClient {
+            self.recaptchaClient = recaptchaClient
+          }
+          if let error = error {
+            print("RecaptchaClient creation error: \(error).")
+          }
+        }
+        resolve(nil)
+         // reject("INITIALIZE_FAILED", "RecaptchaClient init failed by unknown reason", nil)
+    }
+    
+    @objc(executeAction:withResolver:withRejecter:)
+    func executeAction(actionName: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve("empty token")
+    }
 }
